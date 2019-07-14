6e9942883806336f4f13da86e47d601d1ae4a49d
shibuya-r <shibuyaryusuke@gmail.com>
2019-07-14 18:42:04 +0900

    first commit

---
+++ js/firebase-config.js
@@ -0,0 +1,10 @@
+(function () {
+    window.fbConfig = window.fbConfig || {};
+
+    window.fbConfig.config = {
+        apiKey: "AIzaSyATeyn6y19iPGdIv6EWvuhCv0yhUEGH6B0",
+        databaseURL: "https://tk-page.firebaseio.com",
+        projectId: "tk-page",
+        storageBucket: "tk-page.appspot.com"
+    };
+})();
