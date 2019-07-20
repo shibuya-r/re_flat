36a15f42de1b7018218214b0a65bbae9ede540a3
RYO KAINUMA <kr49kr21@gmail.com>
2019-07-21 00:35:01 +0900

    add lesson request function to insert data into cloud firestore, so main.js and index.html are edited

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// Select courses from DB
 $(function () {
     $('#mycalendar').monthly();
 
@@ -14,3 +15,34 @@ $(function () {
     });
 
 });
+
+// Innsert lessons to DB
+$(function () {
+
+    db = firebase.firestore();
+    $("#send-btn").on('click', function () {
+        let selected_date = $("#select_date").val();
+        let selected_course = $("#select_course").val();
+        let num_of_people = $("#num_of_people").val();
+        let name_of_person = $("#name_of_person").val();
+        let email = $("#email").val();
+
+        db.collection("REQUESTED_LESSONS").add({
+        course: selected_course,
+        date: new Date(selected_date),
+        email: email,
+        name_of_person: name_of_person,
+        num_of_people: num_of_people,
+        timestamp: firebase.firestore.FieldValue.serverTimestamp()
+        })
+        .then(function(docRef) {
+            console.log("Document written with ID: ", docRef.id);
+        })
+        .catch(function(error) {
+            console.error("Error adding document: ", error);
+        });
+    });
+});
+
+
+
