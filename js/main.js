18d8fe2b05ac2078f1a84e53ce6956590096e504
RYO KAINUMA <kr49kr21@gmail.com>
2019-07-21 11:25:40 +0900

    edit main.js to add function of decrement number of lesson availabilities on that day after checking firestore

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-// Select courses from DB
 $(function () {
     $('#mycalendar').monthly();
 
@@ -18,7 +17,6 @@ $(function () {
 
 // Innsert lessons to DB
 $(function () {
-
     db = firebase.firestore();
     $("#send-btn").on('click', function () {
         let selected_date = $("#select_date").val();
@@ -27,19 +25,42 @@ $(function () {
         let name_of_person = $("#name_of_person").val();
         let email = $("#email").val();
 
-        db.collection("REQUESTED_LESSONS").add({
-        course: selected_course,
-        date: new Date(selected_date),
-        email: email,
-        name_of_person: name_of_person,
-        num_of_people: num_of_people,
-        timestamp: firebase.firestore.FieldValue.serverTimestamp()
-        })
-        .then(function(docRef) {
-            console.log("Document written with ID: ", docRef.id);
-        })
-        .catch(function(error) {
-            console.error("Error adding document: ", error);
+        var availabilityRef = db.collection('AVAILABILITY').doc(selected_date);
+        availabilityRef.get().then(function(doc) {
+        if (doc.exists) {
+            update_num = doc.data().remaining_num-1
+            if (update_num >= 0) {
+                availabilityRef.update({
+                    remaining_num: update_num
+                })
+                console.log("Update remaining_num:", update_num);
+
+                db.collection("REQUESTED_LESSONS").add({
+                course: selected_course,
+                date: new Date(selected_date),
+                email: email,
+                name_of_person: name_of_person,
+                num_of_people: num_of_people,
+                timestamp: firebase.firestore.FieldValue.serverTimestamp()
+                })
+                .then(function(docRef) {
+                    console.log("Document written with ID: ", docRef.id);
+                    alert('申し込みが完了しました。')
+                    location.reload();
+                })
+                .catch(function(error) {
+                    console.error("Error adding document: ", error);
+                });
+
+            } else {
+                console.log('update_num should be zere or greater')
+            }
+
+        } else {
+            console.log("No such document!");
+        }
+        }).catch(function(error) {
+            console.log("Error getting document:", error);
         });
     });
 });
